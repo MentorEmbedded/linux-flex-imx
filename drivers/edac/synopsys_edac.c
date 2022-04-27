4056b9be7cd02fbd59ed98691341c0354a99945c
Sherry Sun <sherry.sun@nxp.com>
2022-04-27 09:51:37 +0800

    EDAC/synopsys: Re-enable the error interrupts on v3 hw
    [ Upstream commit 4bcffe941758ee17becb43af3b25487f848f6512 ]
    
    zynqmp_get_error_info() writes 0 to the ECC_CLR_OFST register after
    an interrupt for a {un-,}correctable error is raised, which disables
    the error interrupts. Then the interrupt handler will be called only
    once. Therefore, re-enable the error interrupt line at the end of
    intr_handler() for v3.x Synopsys EDAC DDR.
    
    Fixes: f7824ded4149 ("EDAC/synopsys: Add support for version 3 of the Synopsys EDAC DDR")
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Shubhrajyoti Datta <Shubhrajyoti.datta@xilinx.com>
    Acked-by: Michal Simek <michal.simek@xilinx.com>
    Cc: <stable@vger.kernel.org>
    Link: https://lore.kernel.org/r/20220427015137.8406-3-sherry.sun@nxp.com
    Stable-dep-of: 35e6dbfe1846 ("EDAC/synopsys: Fix error injection on Zynq UltraScale+")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/edac/synopsys_edac.c
+++ drivers/edac/synopsys_edac.c
@@ -647,6 +647,28 @@ static irqreturn_t intr_handler_imx8mp(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
+static void enable_intr(struct synps_edac_priv *priv)
+{
+	/* Enable UE/CE Interrupts */
+	if (priv->p_data->quirks & DDR_ECC_INTR_SELF_CLEAR)
+		writel(DDR_UE_MASK | DDR_CE_MASK,
+		       priv->baseaddr + ECC_CLR_OFST);
+	else
+		writel(DDR_QOSUE_MASK | DDR_QOSCE_MASK,
+		       priv->baseaddr + DDR_QOS_IRQ_EN_OFST);
+
+}
+
+static void disable_intr(struct synps_edac_priv *priv)
+{
+	/* Disable UE/CE Interrupts */
+	if (priv->p_data->quirks & DDR_ECC_INTR_SELF_CLEAR)
+		writel(0x0, priv->baseaddr + ECC_CLR_OFST);
+	else
+		writel(DDR_QOSUE_MASK | DDR_QOSCE_MASK,
+		       priv->baseaddr + DDR_QOS_IRQ_DB_OFST);
+}
+
 /**
  * intr_handler - Interrupt Handler for ECC interrupts.
  * @irq:        IRQ number.
@@ -691,6 +713,9 @@ static irqreturn_t intr_handler(int irq, void *dev_id)
 	/* v3.0 of the controller does not have this register */
 	if (!(priv->p_data->quirks & DDR_ECC_INTR_SELF_CLEAR))
 		writel(regval, priv->baseaddr + DDR_QOS_IRQ_STAT_OFST);
+	else
+		enable_intr(priv);
+
 	return IRQ_HANDLED;
 }
 
@@ -916,34 +941,6 @@ static void disable_intr_imx8mp(struct synps_edac_priv *priv)
 	regval = readl(priv->baseaddr + ECC_CLR_OFST);
 	regval &= ~(DDR_CE_INTR_EN_MASK | DDR_UE_INTR_EN_MASK);
 	writel(regval, priv->baseaddr + ECC_CLR_OFST);
-}
-
-static void enable_intr(struct synps_edac_priv *priv)
-{
-	/* Enable UE/CE Interrupts */
-	if (priv->p_data->quirks & DDR_ECC_IMX8MP)
-		return enable_intr_imx8mp(priv);
-
-	if (priv->p_data->quirks & DDR_ECC_INTR_SELF_CLEAR)
-		writel(DDR_UE_MASK | DDR_CE_MASK,
-		       priv->baseaddr + ECC_CLR_OFST);
-	else
-		writel(DDR_QOSUE_MASK | DDR_QOSCE_MASK,
-		       priv->baseaddr + DDR_QOS_IRQ_EN_OFST);
-
-}
-
-static void disable_intr(struct synps_edac_priv *priv)
-{
-	/* Disable UE/CE Interrupts */
-	if (priv->p_data->quirks & DDR_ECC_IMX8MP)
-		return disable_intr_imx8mp(priv);
-
-	if (priv->p_data->quirks & DDR_ECC_INTR_SELF_CLEAR)
-		writel(0x0, priv->baseaddr + ECC_CLR_OFST);
-	else
-		writel(DDR_QOSUE_MASK | DDR_QOSCE_MASK,
-		       priv->baseaddr + DDR_QOS_IRQ_DB_OFST);
 }
 
 static int setup_irq(struct mem_ctl_info *mci,
