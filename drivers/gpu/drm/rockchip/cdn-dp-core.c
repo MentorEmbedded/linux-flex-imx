3a7b1aeab24ce742e9f7bade05ac8b9f07b0f766
Thomas Zimmermann <tzimmermann@suse.de>
2024-11-05 14:38:16 +0100

    drm/rockchip: cdn-dp: Use drm_connector_helper_hpd_irq_event()
    [ Upstream commit 666e1960464140cc4bc9203c203097e70b54c95a ]
    
    The code for detecting and updating the connector status in
    cdn_dp_pd_event_work() has a number of problems.
    
    - It does not aquire the locks to call the detect helper and update
    the connector status. These are struct drm_mode_config.connection_mutex
    and struct drm_mode_config.mutex.
    
    - It does not use drm_helper_probe_detect(), which helps with the
    details of locking and detection.
    
    - It uses the connector's status field to determine a change to
    the connector status. The epoch_counter field is the correct one. The
    field signals a change even if the connector status' value did not
    change.
    
    Replace the code with a call to drm_connector_helper_hpd_irq_event(),
    which fixes all these problems.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Fixes: 81632df69772 ("drm/rockchip: cdn-dp: do not use drm_helper_hpd_irq_event")
    Cc: Chris Zhong <zyw@rock-chips.com>
    Cc: Guenter Roeck <groeck@chromium.org>
    Cc: Sandy Huang <hjc@rock-chips.com>
    Cc: "Heiko Stübner" <heiko@sntech.de>
    Cc: Andy Yan <andy.yan@rock-chips.com>
    Cc: dri-devel@lists.freedesktop.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-rockchip@lists.infradead.org
    Cc: <stable@vger.kernel.org> # v4.11+
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241105133848.480407-1-tzimmermann@suse.de
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/rockchip/cdn-dp-core.c
+++ drivers/gpu/drm/rockchip/cdn-dp-core.c
@@ -934,8 +934,6 @@ static void cdn_dp_pd_event_work(struct work_struct *work)
 {
 	struct cdn_dp_device *dp = container_of(work, struct cdn_dp_device,
 						event_work);
-	struct drm_connector *connector = &dp->mhdp.connector.base;
-	enum drm_connector_status old_status;
 	struct device *dev = dp->mhdp.dev;
 
 	int ret;
@@ -1000,11 +998,7 @@ static void cdn_dp_pd_event_work(struct work_struct *work)
 
 out:
 	mutex_unlock(&dp->lock);
-
-	old_status = connector->status;
-	connector->status = connector->funcs->detect(connector, false);
-	if (old_status != connector->status)
-		drm_kms_helper_hotplug_event(dp->drm_dev);
+	drm_connector_helper_hpd_irq_event(&dp->mhdp.connector.base);
 }
 
 static int cdn_dp_pd_event(struct notifier_block *nb,
